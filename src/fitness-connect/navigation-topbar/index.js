a2824e67c22f4c38b588726a5d87a14c2be75652
Chenhao Lu <garylu32@gmail.com>
2023-08-08 01:11:24 -0700

    login screen upload

--- src/fitness-connect/navigation-topbar/index.js
+++ src/fitness-connect/navigation-topbar/index.js
@@ -1,14 +1,25 @@
-
 import { Link } from "react-router-dom"
 
 function NavigationTopBar() {
     return (
-        <nav className="nav nav-tabs mb-2">
-            <Link className="nav-link" to="/fitness-connect/home">Home</Link>
-            <Link className="nav-link" to="/fitness-connect/login">Login</Link>
-            <Link className="nav-link" to="/fitness-connect/register">Register</Link>
+        <nav className="navbar navbar-light bg-light justify-content-between mt-2 mb-2">
+            <div className="navbar-brand ms-3">
+                <a className="nav-link" href="/fitness-connect/home">FITNESS CONNECT</a>
+            </div>
+
+            {/* <Link className="nav-link" to="/fitness-connect/home">Home</Link> */}
+
+            <div className="nav">
+                <Link className="nav-link" to="/fitness-connect/login">Login</Link>
+                <Link className="nav-link" to="/fitness-connect/register">Register</Link>
+            </div>
         </nav>
+
+
     );
 }
 
 export default NavigationTopBar;
+
+
+
--- src/fitness-connect/user/login-screen.js
+++ src/fitness-connect/user/login-screen.js
@@ -1,14 +1,65 @@
-import React from "react";
+import React, { useState } from "react";
 import NavigationTopBar from "../navigation-topbar";
 
 function LoginSreen() {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        // Here, you can call your server-end logic to authenticate the user
+        // For example:
+        // authenticateUser(email, password).then(response => { ... });
+
+        console.log("Email:", email, "Password:", password);
+    };
     return (
         <div>
             <NavigationTopBar />
-            <h1>Login Screen</h1>
+            <div className="container d-flex justify-content-center align-items-center">
+                <div className="row justify-content-center">
+                    <div >
+                        <div className="card mt-5" style={{ width: "24rem" }}>
+                            <img
+                                className="card-img-top"
+                                src={require('../../images/login_sample.jpeg')}// image path
+                                alt="login image cap"
+                            />
+                            <div className="card-body">
+                                <h5 className="card-title">Login</h5>
+                                <form onSubmit={handleSubmit}>
+                                    <div className="mb-3">
+                                        <label htmlFor="email" className="form-label">Email address</label>
+                                        <input
+                                            type="email"
+                                            className="form-control"
+                                            id="email"
+                                            value={email}
+                                            onChange={(e) => setEmail(e.target.value)}
+                                            required
+                                        />
+                                    </div>
+                                    <div className="mb-3">
+                                        <label htmlFor="password" className="form-label">Password</label>
+                                        <input
+                                            type="password"
+                                            className="form-control"
+                                            id="password"
+                                            value={password}
+                                            onChange={(e) => setPassword(e.target.value)}
+                                            required
+                                        />
+                                    </div>
+                                    <button type="submit" className="btn btn-primary">Login</button>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
-    )
-
-}
+    );
+};
 
 export default LoginSreen;
